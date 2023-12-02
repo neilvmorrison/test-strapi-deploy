b35e44875908ffcb62ef4ffae874aa5493b7ebcd
morrisonneilv <morrison.neil.v@gmail.com>
2023-12-02 15:28:25 -0500

    email shit

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "amazon-ses",
+      providerOptions: {
+        key: env("AWS_SES_KEY"),
+        secret: env("AWS_SES_SECRET"),
+        amazon: "https://email.us-east-1.amazonaws.com",
+      },
+      settings: {
+        defaultFrom: "myemail@protonmail.com",
+        defaultReplyTo: "myemail@protonmail.com",
+      },
+    },
+  },
+});
